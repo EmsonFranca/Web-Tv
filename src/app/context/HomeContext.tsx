c0193c668b8e518fd5a960e18d8d1f1ac87ecc4c
EmsonFranca <esf12364@gmail.com>
2024-03-19 15:26:41 -0300

    first commit

---
+++ src/app/context/HomeContext.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { ReactNode, RefObject, createContext, useRef, useState } from "react";
+
+type HomeContextData = {
+    videoURL: string;
+    playing: boolean;
+    totalTime : number;
+    currentTime: number;
+
+    videoRef: RefObject<HTMLVideoElement>;
+    playPause: () => void;
+    configCurrentTime: (time:number) => void;
+}
+
+export const HomeContext =
+   createContext({} as HomeContextData);
+
+type ProviderProps = {
+    children: ReactNode;    
+}
+
+const HomeContextProvider = ({children}: ProviderProps) => {
+    const [videoURL, setVideoURL] = useState("video/video01.mp4");
+    const [playing, setPlaying] = useState(false);
+    const [totalTime, setTotalTime] = useState(1000)
+    const [currentTime, setCurrentTime] = useState(0);
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const configCurrentTime = (time: number) => {
+        const video = videoRef.current;
+        if(!video) return;
+        video.currentTime = time;
+        setCurrentTime(time)
+    }
+    const playPause = ()  => {
+        console.log("OPaa");
+        
+        const video = videoRef.current;
+        if (!video) return;
+
+        if (playing) {
+           video.pause();     
+        }
+        else {
+            video.play();
+        }
+        setPlaying(!playing);
+    }
+
+    return (
+        <HomeContext.Provider value={
+            {
+                videoURL,
+                playing,
+                videoRef,
+                playPause,
+                totalTime,
+                currentTime,
+                configCurrentTime 
+                
+            }
+        }>
+         {children}
+        </HomeContext.Provider>
+    )
+}
+
+
+export default HomeContextProvider;
---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useContext } from "react";
+import { HomeContext } from "./context/HomeContext";
+
+export default function Home() {
+  const {
+    videoURL,
+    playing,
+    videoRef,
+    playPause,
+    totalTime,
+    currentTime,
+    configCurrentTime
+  } = useContext(HomeContext);
+  return (
+    <main className="">
+      <ul className="bg-gray-800 text-opacity-10 p-4 ">
+        <li className="font-bold text-center text-zinc-50">
+          URL: {videoURL} - {playing ? "true" : "false"}
+        </li>
+      </ul>
+
+      <div className=" bg-gray-950 text-center  ">
+        <div className="flex justify-center">
+          <video ref={videoRef} className="pt-4" controls src="video/video01.mp4"></video>
+        </div>
+        <div className="flex-col flex items-center pt-2 gap-1">
+          <input 
+            type="range" 
+            min={0} 
+            max={totalTime} 
+            value={currentTime} 
+            onChange={
+              (e)=>configCurrentTime(Number(e.target.value))}
+          />
+          <button className="bg-orange-400 p-2 mt-4 rounded-xl mb-4" onClick={playPause} >
+            {/* { playing ? <FaPause> : <Faplay> } */}
+              play | pause 
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
