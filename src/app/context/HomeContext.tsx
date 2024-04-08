353598d39db140fb58bee654fb4b0d6c6c4732fd
EmsonFranca <esf12364@gmail.com>
2024-04-08 17:31:56 -0300

    add full screen the videos

--- src/app/context/HomeContext.tsx
+++ src/app/context/HomeContext.tsx
@@ -1,104 +1,166 @@
-"use client";
+'use client'
 
-import {
-  ReactNode,
-  RefObject,
-  createContext,
-  useRef,
-  useState,
-  useEffect,
-} from "react";
-// importando os videos
+import { ReactNode, RefObject, createContext, useEffect, useRef, useState } from "react";
 import videos, { Video } from "../data/videos";
 
+abstract class Filter {
+    red : number;
+    green: number;
+    blue: number;
+
+    constructor() {
+        this.red = 0;
+        this.green = 0;
+        this.blue = 0;
+
+    }
+    abstract calc(red : number, green: number, blue: number):void
+}
+
+class GreenFilter extends Filter {
+    calc(red: number, green: number, blue: number): void {
+        this.red = 0;
+        this.green = green;
+        this.blue  = 0;
+    }
+
+}
+class RedFilter extends Filter {
+    calc(red: number, green: number, blue: number): void {
+        this.red = red
+        this.green = 0
+        this.blue = 0
+    }
+}
+
 type HomeContextData = {
-  videoURL: string;
-  playing: boolean;
-  totalTime: number;
-  currentTime: number;
+    videoURL: string;
+    playing: boolean;
+    totalTime: number;
+    currentTime: number;
+    videoRef: RefObject<HTMLVideoElement>;
+    canvasRef: RefObject<HTMLCanvasElement>;
+    playPause: () => void;
+    configCurrentTime: (time:number) => void;
+    configVideo: (index: number) => void;
+}
 
-  videoRef: RefObject<HTMLVideoElement>;
-  playPause: () => void;
-  configCurrentTime: (time: number) => void;
-};
-
-export const HomeContext = createContext({} as HomeContextData);
+export const HomeContext =
+   createContext({} as HomeContextData);
 
 type ProviderProps = {
-  children: ReactNode;
-};
+    children: ReactNode;    
+}
 
-const HomeContextProvider = ({ children }: ProviderProps) => {
-  const [videoURL, setVideoURL] = useState("");
-  const [videoIndex, setVideoIndex] = useState(0);
-  const [playing, setPlaying] = useState(false);
-  const [totalTime, setTotalTime] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
-  const videoRef = useRef<HTMLVideoElement>(null);
+const HomeContextProvider = ({children}: ProviderProps) => {
+    const [videoURL, setVideoURL] = useState("");
+    const [videoIndex, setVideoIndex] = useState(0);
+    const [playing, setPlaying] = useState(false);
+    const [totalTime, setTotalTime] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null)
 
-  useEffect(() => {
-    configVideo(videoIndex)
-  }, []);
+    useEffect(()=>{
+        configVideo(videoIndex);
+    }, []);
 
-  const configVideo = (index: number) => {
-    const currentIndex = index % videos.length;
-    const currentVideo: Video = videos[currentIndex];
-    const currentVideoURL = currentVideo.videoURL;
-    setVideoURL(currentVideoURL);
-    setVideoIndex(currentIndex)
-  }
-
-  useEffect(() => {
-    const video = videoRef.current;
-    if (video) {
-      video.onloadedmetadata = () => {
-        setTotalTime(video.duration);
-        setCurrentTime(video.currentTime);
-      }; 
-
-      if(playing) {
-        video.play;
-      }
-
-      video.onended = () => {
-        configVideo(videoIndex + 1)
-      }
+    const configVideo = (index: number) => {
+        const currentIndex = index % videos.length;
+        const currentVideo: Video = videos[currentIndex];
+        const currentVideoURL = currentVideo.videoURL;
+        setVideoURL(currentVideoURL);
+        setVideoIndex(currentIndex);
     }
-  }), [videoURL];
 
-  const configCurrentTime = (time: number) => {
-    const video = videoRef.current;
-    if (!video) return;
-    video.currentTime = time;
-    setCurrentTime(time);
-  };
-  const playPause = () => {
-    const video = videoRef.current;
-    if (!video) return;
+    useEffect(() => {
+        const video = videoRef.current;
+        if (video) {
+            video.onloadedmetadata = () => {
+                setTotalTime(video.duration);
+                setCurrentTime(video.currentTime);
 
-    if (playing) {
-      video.pause();
-    } else {
-      video.play();
+                if (playing) {
+                    video.play();
+                }
+            }
+            // move input range
+            video.ontimeupdate = () => {
+                const video = videoRef.current;
+                if(!video) return;
+                setCurrentTime(video.currentTime);
+            }
+
+            video.onended = () => {
+                configVideo(videoIndex + 1);
+            }
+        }
+
+    }, [videoURL]);
+
+    const configCurrentTime = (time: number) => {
+        const video = videoRef.current;
+        if (!video) return;
+        video.currentTime = time;
+        setCurrentTime(time);
     }
-    setPlaying(!playing);
-  };
 
-  return (
-    <HomeContext.Provider
-      value={{
-        videoURL,
-        playing,
-        videoRef,
-        playPause,
-        totalTime,
-        currentTime,
-        configCurrentTime,
-      }}
-    >
-      {children}
-    </HomeContext.Provider>
-  );
-};
+    const playPause = ()  => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        if (playing) {
+           video.pause();     
+        }
+        else {
+            video.play();
+            draw();
+        }
+        setPlaying(!playing);
+    }
+// modo teatro
+    const draw = () => {
+        const video = videoRef.current;
+        const canvas = canvasRef.current;
+        if(!video || !canvas) return;
+
+        let context = canvas.getContext("2d");
+        context?.drawImage(video, 0,0, canvas.width, canvas.height);
+        const imageData = context.getImageData(0,0 ,canvas.width, canvas.height);
+        const data = imageData.data;
+        const filter: Filter = new GreenFilter()
+        for(let i = 0; i < data.length; i +=4){
+            const red = data[i+0]
+            const green = data[i+1]
+            const blue = data[i+2]
+
+            filter.calc(red,green,blue)
+            data[i + 0] = filter.red
+            data[i + 1] = filter.green
+            data[i + 2] = filter.blue
+        }
+
+        context?.putImageData(imageData, 0,0)
+
+        requestAnimationFrame(draw);
+    }
+    return (
+        <HomeContext.Provider value={
+            {
+                videoURL,
+                playing,
+                totalTime,
+                currentTime,
+                videoRef,
+                canvasRef,
+                playPause,
+                configCurrentTime,
+                configVideo
+            }
+        }>
+         {children}
+        </HomeContext.Provider>
+    )
+}
 
 export default HomeContextProvider;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,29 +10,39 @@ export default function Home() {
     videoURL,
     playing,
     videoRef,
+    canvasRef,
     playPause,
     totalTime,
     currentTime,
     configCurrentTime,
+    configVideo
   } = useContext(HomeContext);
   return (
-    <main className="">
-      <div className="bg-gray-800 text-opacity-10 p-4 ">
-        <h1 className="font-bold text-center text-zinc-50">
-          Titulo: 
-        </h1>
-      </div>
+    <main className="mx-auto w-[100%] flex">
 
-      <div className=" bg-gray-950 text-center ">
-        <div className="flex justify-center">
+      <div className="w-[65%] mr-1 bg-gray-950 text-center ">
+        <div className="">
           <video
             ref={videoRef}
-            className="pt-4"
+            className="pt-4 mt-1"
             controls
             src={videoURL}
+            hidden
           ></video>
         </div>
-        <div className="flex-col flex items-center pt-2 gap-1">
+          <canvas className="w-full has-[380px]" ref={canvasRef}></canvas> 
+        <div className="w-[35%]" >
+          {
+             videos.map((video:Video, index) => {
+              return (
+                <button key={index} className="w-full" onClick={(e) => configVideo(index)}>
+                  <img  className="w-full h-[200px] mb-1" src={video.imageURL}></img>
+                </button>
+              )
+            })
+          }
+        </div>
+        <div className="pt-2 gap-1">
           <input
             type="range"
             min={0}
@@ -47,7 +57,9 @@ export default function Home() {
             {playing ? <FaPause /> : <FaPlay />}
           </button>
         </div>
+        
       </div>
     </main>
   );
 }
+
